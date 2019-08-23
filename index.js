dd8a45f70516271c40dadc92ed98339c3eeee475
Diego <d.gimenez0101@gmail.com>
2019-08-23 09:35:37 -0300

    Imported Chart

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-fetch('')
+var res
+
+fetch('https://randomuser.me/api/')
 .then(response => response.json())
-.then(json => console.log(json))
+.then(json => res = json)
+
+var ctx = document.getElementById('canvas');
+
+var myChart = new Chart(ctx, {
+    type: line,
+    data: {
+        labels: ['i', 'wd', 'ws', 'r'],
+        datasets: [{
+            label: 'Grafico de linea',
+            backgroundColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2]
+        }]
+    },
+
+    options: {}
+});
