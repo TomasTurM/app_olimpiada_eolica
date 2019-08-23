3470ea3cd5b2fba46c6cf63108da79e8edb0b332
TomasM <tomasm@tomassystem3.0>
2019-08-23 11:38:24 -0300

    php progression

--- db_eolica.sql
+++ db_eolica.sql
@@ -3,7 +3,7 @@ CREATE DATABASE IF NOT EXISTS db_eolica;
 USE db_eolica;
 
 CREATE TABLE IF NOT EXISTS datos_eolica (
-	dato_id = TIMESTAMP NOT NULL,
+	dato_id TIMESTAMP NOT NULL,
 	i DOUBLE(3,2),
 	w_d CHAR(30),
 	w_s DOUBLE(3,2),
--- db_save.php
+++ db_save.php
@@ -1,34 +1,51 @@
 <?php
+	# Ejecuccion del request.py
+	$exec_py = escapeshellcmd('./request.py');
+	$output = shell_exec($exec_py);
+	$jsonArray = json_decode($output, true);
+  	print_r($jsonArray);
+
+  	/*
 	# Datos del POST
+	$dato_id = $_POST['dato_id'];
 	$i = $_POST['i'];
 	$w_d = $_POST['w_d'];
 	$w_s = $_POST['w_s'];
 	$v = $_POST['v'];
+	*/
 
 	# Coneccion con DB
 	$con = mysqli_connect('localhost','admin','pepe1234');
 	if (!$con) {
     	die('Could not connect: ' . mysqli_error($con));
 	}
+
+	# Creacion de la database
+	$command = "mysql --user=root --password=pepe1234 " . "< ./db_eolica.sql"
+	mysqli_query($con,$command)
+
+	# Seleccion de DB
 	mysqli_select_db($con,'db_eolica');
 
-	# Testeo en .txt
-	$myFile = "db.txt";
-	$fh = fopen($myFile, 'w') or die("can't open file");
-	fwrite($fh, $return);
-	fclose($fh);
-
 	# Guardar en DB
-	if (file_exists('db.sql')) {
-		$sql = "USE "
+  	# Loop through Array
+  	foreach ($jsonArray as $key => $value) {
+    	# echo $value["name"] . ", " . $value["gender"] . "<br>";
+  		$sql = "INSERT INTO datos_eolica (dato_id, i, w_d, w_s, v)
+		VALUES ($value["dato_id"], $value["i"], $value["w_d"],
+		$value["w_s"], $value["v"])"
+  	}
+
+  	/*
+	$sql = "INSERT INTO datos_eolica (dato_id, i, w_d, w_s, v)
+	VALUES ($dato_id, $i, $w_d, $w_s, $v)"
+	
+	if (mysqli_query($con, $sql)) {
+	    echo "New record created successfully";
 	} else {
-		$sql = "CREATE DATABASE db_eolica";
-		if (mysqli_query($con, $sql)) {
-		    echo "Database created successfully";
-		} else {
-		    echo "Error creating database: " . mysqli_error($con);
-		}
+	    echo "Error: " . $sql . "<br>" . mysqli_error($con);
 	}
+	*/
 
 	mysqli_close($con);
 ?>
